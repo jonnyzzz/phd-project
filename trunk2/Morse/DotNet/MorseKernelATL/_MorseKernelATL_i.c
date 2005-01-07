e0317c413ceec86c23018ebafa9e338177155a35
jo <jo@6a918f4f-bbb9-b144-964e-14738c76912e>
2005-01-07 02:57:08 +0000

    build
    git-svn-id: file:///vagrant/svn/dsis@145 6a918f4f-bbb9-b144-964e-14738c76912e

--- trunk2/Morse/DotNet/MorseKernelATL/_MorseKernelATL_i.c
+++ trunk2/Morse/DotNet/MorseKernelATL/_MorseKernelATL_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 6.00.0361 */
-/* at Sun Jan 02 19:30:36 2005
+/* at Fri Jan 07 05:15:39 2005
  */
 /* Compiler settings for _MorseKernelATL.idl:
     Oicf, W1, Zp8, env=Win32 (32b run)
