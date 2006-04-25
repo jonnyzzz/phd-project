231bf2157160c57cca50bc4afbea6062584f8fa7
jo <jo@6a918f4f-bbb9-b144-964e-14738c76912e>
2006-04-25 07:09:28 +0000

    Delay 2.21
    git-svn-id: file:///vagrant/svn/dsis@354 6a918f4f-bbb9-b144-964e-14738c76912e

--- trunk2/Morse/DotNet/MorseKernel2/_MorseKernel2_i.c
+++ trunk2/Morse/DotNet/MorseKernel2/_MorseKernel2_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 6.00.0361 */
-/* at Tue Apr 11 00:40:48 2006
+/* at Tue Apr 18 11:57:24 2006
  */
 /* Compiler settings for _MorseKernel2.idl:
     Oicf, W1, Zp8, env=Win32 (32b run)
