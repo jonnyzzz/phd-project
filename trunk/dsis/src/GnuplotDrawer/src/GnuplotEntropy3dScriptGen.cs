a4ccab022239128eedbc4f7caa7d1d3e9c0d5ee4
jonny <jonny@6a918f4f-bbb9-b144-964e-14738c76912e>
2007-12-05 12:57:21 +0000

    git-svn-id: file:///vagrant/svn/dsis@381 6a918f4f-bbb9-b144-964e-14738c76912e

--- trunk/dsis/src/GnuplotDrawer/src/GnuplotEntropy3dScriptGen.cs
+++ trunk/dsis/src/GnuplotDrawer/src/GnuplotEntropy3dScriptGen.cs
@@ -1,27 +1,52 @@
+
+using DSIS.Core.Visualization;
+
 namespace DSIS.GnuplotDrawer
 {
-  public class GnuplotEntropy3dScriptGen : Gnuplot3dScriptGen
+  public class GnuplotEntropy3dScriptGen : PngWriterBase, IGnuplotPhaseScriptGen
   {
     private bool myIsFirstFile = true;
 
     public GnuplotEntropy3dScriptGen(string filename, GnuplotScriptParameters @params)
       : base(filename, @params)
     {      
+      myWriter.Write("splot ");
     }
 
-    public override void AddPointsFile(GnuplotPointsFileWriter file)
+    public void AddPointsFile(GnuplotPointsFileWriter file)
     {
       if (myIsFirstFile)
         myIsFirstFile = false;
       else
         myWriter.WriteLine(", \\");
 
-      myWriter.Write(" '{0}' using 1:2:(0):1:2:3 with line title \"{1}\" with ", file.Filename, string.Format("Count {0}", file.PointsCount));
+      string lines = file.Filename + ".lines";
+      string white = file.Filename + ".zero";
 
-      if (myParams.ForcePoints || file.PointsCount < 300)
-        myWriter.Write(" points ");
-      else
-        myWriter.Write(" dots ");
-    }    
+      using(GnuplotPointsFileWriter linesWriter = new GnuplotPointsFileWriter(lines, 3))
+      {        
+        using(GnuplotPointsFileWriter zeroPlane = new GnuplotPointsFileWriter(white, 3))
+        {
+          foreach (ImagePoint point in new GnuplotPointsFileReader(file.Filename).Read())
+          {
+            ImagePoint bs = new ImagePoint(point.Point[0], point.Point[1], 0);
+            zeroPlane.WritePoint(bs);
+
+            linesWriter.WritePoint(bs);
+            linesWriter.WritePoint(point);
+            linesWriter.WritePoint(bs);
+          }
+        }
+      }
+
+      myWriter.Write(" '{0}' with lines, '{1}' with lines lc rgb \"#ffffff\" ", lines, white);
+    }
+
+    public override void Dispose()
+    {
+      myWriter.WriteLine(" ;");
+      base.Dispose();
+    }
+
   }
 }
