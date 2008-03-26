a55ce9253dcfa7855e7c47b7f412bda31a2fc928
jonny <jonny@6a918f4f-bbb9-b144-964e-14738c76912e>
2008-03-26 22:21:35 +0000

    new project
    git-svn-id: file:///vagrant/svn/dsis@464 6a918f4f-bbb9-b144-964e-14738c76912e

---
+++ trunk/dsis/src/Graph.Entropy.Intersection/src/PointMethodIntersectionMeasure.cs
@@ -0,0 +1,17 @@
+using DSIS.Core.Coordinates;
+using DSIS.Core.System;
+
+namespace DSIS.Graph.Entropy.Intersection.src
+{
+  public class PointMethodIntersectionMeasure<Q> 
+    where Q : ICellCoordinate
+  {
+    private readonly IGraph<Q> myGraph;
+    private readonly ISystemInfo myFunction;
+
+    private void Start()
+    {
+      
+    }
+  }
+}
