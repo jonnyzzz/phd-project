62cd23b4f2fa64b428ea230dca6780cef79e75f6
jonny <jonny@6a918f4f-bbb9-b144-964e-14738c76912e>
2008-12-26 21:26:58 +0000

    compiles
    git-svn-id: file:///vagrant/svn/dsis@683 6a918f4f-bbb9-b144-964e-14738c76912e

--- trunk/dsis/src/UI.Wizard/src/JoinedWizardPack.cs
+++ trunk/dsis/src/UI.Wizard/src/JoinedWizardPack.cs
@@ -1,43 +1,189 @@
+using System;
 using System.Collections.Generic;
-using DSIS.Utils;
+using log4net;
 
 namespace DSIS.UI.Wizard
 {
-  public class JoinedWizardPack //: IWizardPack
+  public class JoinedWizardPack : IWizardPack
   {
-    //TODO:  Implelent me
-    private readonly List<IWizardPack> myWizards;
+    private static readonly ILog LOG = LogManager.GetLogger(typeof (JoinedWizardPack));
 
-    public JoinedWizardPack(List<IWizardPack> wizards)
+    private WizardChainNode myCurrentWizardNode;
+
+    public JoinedWizardPack(string title, IEnumerable<IWizardPack> wizards)
     {
-      myWizards = wizards;
+      myCurrentWizardNode = WizardChainNode.Create(wizards);
+      Title = title;
+      if (wizards == null)
+      {
+        throw new ArgumentException("Can not create joined wizard from 0 wizards");
+      }
     }
 
-    public string Title { get; set;}
+    public string Title { get; private set; }
 
     public IWizardPage FirstPage
     {
-      get { return myWizards.GetFirst().FirstPage; }
+      get { return myCurrentWizardNode.First.Page; }
     }
 
     public bool IsLastPage(IWizardPage page)
     {
-      throw new System.NotImplementedException();
+      return myCurrentWizardNode.Last.IsLastPage(page);
     }
 
     public IWizardPage Next(IWizardPage page)
     {
-      throw new System.NotImplementedException();
+      if (myCurrentWizardNode.IsLastPage(page))
+      {
+        if (myCurrentWizardNode.Next == null)
+        {
+          return null;
+        }
+
+        myCurrentWizardNode = myCurrentWizardNode.Next;
+        return myCurrentWizardNode.Page;
+      }
+      return myCurrentWizardNode.NextPage();
     }
 
     public void OnFinish()
     {
-      throw new System.NotImplementedException();
+      myCurrentWizardNode.Each(x => Try(x, "OnCancel", () => x.OnCancel()));
     }
 
     public void OnCancel()
     {
-      throw new System.NotImplementedException();
+      myCurrentWizardNode.Each(x => Try(x, "OnCancel", () => x.OnCancel()));
+    }
+
+    private static void Try(IWizardPack pack, string name, Action a)
+    {
+      try
+      {
+        a();
+      }
+      catch (Exception e)
+      {
+        LOG.Error(
+          string.Format("Failed to process action {0} on wizard {1} ({2}). {3}", name, pack.Title, pack.GetType(), e.Message), e);
+      }
+    }
+
+    public void PageShown(IWizardPage page)
+    {
+      myCurrentWizardNode.PageShown(page);
+    }
+
+    private class JoinedPage
+    {
+      public readonly IWizardPack myWizard;
+      public readonly IWizardPage myPage;
+
+      public JoinedPage(IWizardPack wizard, IWizardPage page)
+      {
+        myWizard = wizard;
+        myPage = page;
+      }
+    }
+
+    private class WizardChainNode
+    {
+      public readonly IWizardPack Pack;
+      public readonly WizardChainNode Next;
+      public readonly WizardChainNode Prev;
+
+      public IWizardPage Page{ get; private set;}
+
+      private WizardChainNode(WizardChainNode prev, IEnumerator<IWizardPack> enu)
+      {        
+        Prev = prev;
+        Pack = enu.Current;
+        Next = enu.MoveNext() ? new WizardChainNode(this, enu) : null;
+        Page = Pack.FirstPage;
+      }
+
+      public static WizardChainNode Create(IEnumerable<IWizardPack> enu)
+      {
+        using(var en = enu.GetEnumerator())
+        {
+          if (en.MoveNext())
+          {
+            return new WizardChainNode(null, en);
+          }
+          return null;
+        }
+      }
+
+      public void AssertCurrentPage(IWizardPage page) {
+      }
+
+      public bool IsLastPage(IWizardPage page)
+      {
+        throw new NotImplementedException();
+      } 
+
+      public WizardChainNode First
+      {
+        get { return Prev == null ? this : Prev.First; }
+      }
+
+      public WizardChainNode Last
+      {
+        get { return Next == null ? this : Next.Last; }
+      }
+
+      public void Each(Action<IWizardPack> page)
+      {
+        for(var node = First; node != null; node = node.Next)
+        {
+          try
+          {
+            page(node.Pack);
+          } catch(Exception e)
+          {
+            LOG.Error(string.Format("Failed to proceed action on {0}. {1}", node.Pack, e.Message), e);
+          }
+        }
+      }
+
+      public IWizardPage NextPage()
+      {
+        Page = Pack.Next(Page);
+        if (Page == null)
+        {
+          throw new ArgumentException("Page can not be null. Use IsLastPage to check it");
+        }
+        return Page;
+      }
+
+      public void PageShown(IWizardPage page)
+      {
+        AssertCurrentPage(page);
+        Pack.PageShown(page);
+      }
+    }
+
+    private class JoinedWizardCollection : List<JoinedPage>
+    {
+      public IWizardPack FindForPage(IWizardPage page)
+      {
+        foreach (var joinedPage in this)
+        {
+          if (Equals(joinedPage.myPage))
+          {
+            return joinedPage.myWizard;
+          }
+        }
+        return null;
+      }
+
+      public IWizardPage CreatePage(IWizardPack pack, Func<IWizardPack, IWizardPage> fn)
+      {
+        var page = fn(pack);
+        Add(new JoinedPage(pack, page));
+        return page;
+      }
     }
   }
 }
