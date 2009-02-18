5783bbe90988b8177d34591f91094e6fab862172
jonny <jonny@6a918f4f-bbb9-b144-964e-14738c76912e>
2009-02-18 21:52:44 +0000

    version increment + copyright
    git-svn-id: file:///vagrant/svn/dsis@742 6a918f4f-bbb9-b144-964e-14738c76912e

--- trunk/dsis/src/AssemblyInfo.cs
+++ trunk/dsis/src/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyCompany("Eugene Petrenko")]
+[assembly: AssemblyCompany("Eugene Petrenko (eugene.petrenko@gmail.com")]
 [assembly: AssemblyProduct("DSIS")]
-[assembly: AssemblyCopyright("Copyright (C) Eugene Petrenko 2007-2009")]
+[assembly: AssemblyCopyright("Copyright (C) Eugene Petrenko 2007-2009. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,5 +20,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
