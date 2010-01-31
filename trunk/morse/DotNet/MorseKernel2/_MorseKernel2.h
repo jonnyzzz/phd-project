24c62b38a27bc7cef0328c5b593d11ffbef8ffc2
jonny <jonny@6a918f4f-bbb9-b144-964e-14738c76912e>
2010-01-31 20:24:23 +0000

    enforce x86 for all projects
    git-svn-id: file:///vagrant/svn/dsis@807 6a918f4f-bbb9-b144-964e-14738c76912e

--- trunk/morse/DotNet/MorseKernel2/_MorseKernel2.h
+++ trunk/morse/DotNet/MorseKernel2/_MorseKernel2.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0500 */
-/* at Fri Apr 24 23:04:05 2009
+/* at Sun Jan 31 23:20:53 2010
  */
 /* Compiler settings for _MorseKernel2.idl:
     Oicf, W1, Zp8, env=Win32 (32b run)
