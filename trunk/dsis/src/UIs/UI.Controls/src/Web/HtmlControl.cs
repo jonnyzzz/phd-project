3407429e163d0bb145bd3bcabe12465576b1adf0
jonny <jonny@6a918f4f-bbb9-b144-964e-14738c76912e>
2010-06-17 16:04:53 +0000

    drop legacy code, update html control default styles
    git-svn-id: file:///vagrant/svn/dsis@877 6a918f4f-bbb9-b144-964e-14738c76912e

--- trunk/dsis/src/UIs/UI.Controls/src/Web/HtmlControl.cs
+++ trunk/dsis/src/UIs/UI.Controls/src/Web/HtmlControl.cs
@@ -30,6 +30,49 @@ namespace DSIS.UI.Controls.Web
     {
       var doc = new XmlDocument();
       doc.LoadXml("<html><head><title></title></head><body/></html>");
+      
+      var style = doc.CreateElement("style");
+      style.SetAttribute("type", "text/css");
+      style.InnerText = @"
+         html {
+           overflow-y: scroll;
+         }
+
+         body {
+            font-family: havletica, tahoma, verdana, arial, sans-serif;
+            font-size: 12pt;
+            margin: 0;
+            padding: 0;
+            color: #3f3f3f;
+            background: #fff;
+            width:100%;
+            padding-top: 0.2em;
+          }
+
+        h1, h2, h3, h4, h5 {          
+        }
+
+        img {
+          border: none
+        }
+
+        a {
+          color: #ED2C10;
+        }
+
+        a:visited {
+          color: #ED2C10;
+        }
+
+        a:hover {
+          text-decoration: none;
+          color: white;
+          background-color: #ED2C10;
+        }
+      ";
+      var head = doc.SelectSingleNode("html/head");
+      head.AppendChild(style);
+      
       root((XmlElement) doc.SelectSingleNode("html/body"));
 
       var code = doc.OuterXml;
