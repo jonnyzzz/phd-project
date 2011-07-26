85644bb5ecbdfccb979825c16ed99e145c07cd6d
jonny <jonny@6a918f4f-bbb9-b144-964e-14738c76912e>
2011-07-26 19:08:39 +0000

    add image-pixel color entropy project
    git-svn-id: file:///vagrant/svn/dsis@905 6a918f4f-bbb9-b144-964e-14738c76912e

---
+++ trunk/dsis/src/Graphs/Graph.Image/src/GraphFromImageBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using DSIS.Core.System.Impl;
+using DSIS.IntegerCoordinates;
+using EugenePetrenko.Shared.Core.Ioc.Api;
+
+namespace DSIS.Graph.Images
+{
+  public class GraphFromImageBuilder
+  {
+    [Autowire]
+    private IIntegerCoordinateFactory myFactory { get; set; }
+      
+    public IReadonlyGraph BuildGraphFromImage<T>(Image image, Func<Color, double> color, Func<Color, T> hash)
+      where T : IEquatable<T>
+    {
+      var space = new DefaultSystemSpace(2, new[] {0.0, 0.0}, new double[] {image.Width, image.Height}, new long[] {1, 1});
+      var system = myFactory.Create(space, new long[] {image.Width, image.Height});
+
+
+      return null;
+    }
+  }
+}
