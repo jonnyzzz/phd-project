ea0af5b743f30308e5345a637c4ff5dbee7de307
jonny <jonny@6a918f4f-bbb9-b144-964e-14738c76912e>
2011-07-26 21:09:01 +0000

    project patcher
    git-svn-id: file:///vagrant/svn/dsis@906 6a918f4f-bbb9-b144-964e-14738c76912e

---
+++ trunk/dsis/tools/csproj.patcher/csproj.patcher/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+
+namespace csproj.patcher
+{
+  class Program
+  {
+    private const string NS = "http://schemas.microsoft.com/developer/msbuild/2003";
+    static void Main(string[] args)
+    {
+      var root = args.FirstOrDefault() ?? Environment.CurrentDirectory;
+      foreach (var proj in Directory.GetFiles(root, "*.csproj", SearchOption.AllDirectories))
+      {
+        var doc = new XmlDocument();
+        using(var rdr = File.OpenRead(proj))
+          doc.Load(rdr);
+
+        var ns = new XmlNamespaceManager(doc.NameTable);
+        ns.AddNamespace("m", NS);
+
+        XmlElement addParent = null;
+        var assemblies = new HashSet<string>{
+          "System",
+          "System.Core",
+          "System.Xml.Linq",
+          "System.Data.DataSetExtensions",
+          "System.Data",
+          "System.Xml"};
+        foreach (XmlAttribute attr in doc.SelectNodes("m:Project/m:ItemGroup/m:Reference/@Include", ns))
+        {
+          assemblies.Remove(attr.Value);
+          XmlNode p = attr.OwnerElement;
+          for (; p != null && !(p is XmlElement); p = p.ParentNode) ;
+          if (p != null)
+            addParent = (XmlElement) p;
+        }
+
+        if (addParent == null)
+        {
+          var ancor = doc.SelectNodes("m:Project/m:Import", ns).Cast<XmlElement>().First();
+          var el = doc.CreateElement("ItemGroup", NS);
+          addParent = (XmlElement) ancor.ParentNode.InsertBefore(el, ancor);
+        }
+
+        if (assemblies.Count == 0) continue;
+
+        foreach (var assembly in assemblies)
+        {
+          var el = doc.CreateElement("Reference", NS);
+          el.SetAttribute("Include", assembly);
+          addParent.AppendChild(el);
+        }
+
+        doc.Save(proj);
+        Console.Out.WriteLine(proj);
+      }
+    }
+  }
+}
