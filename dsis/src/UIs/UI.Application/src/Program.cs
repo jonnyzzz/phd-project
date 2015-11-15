3fae402802c148144c3ad1cb57c18348db9a67a8
Eugene Petrenko <eugene.petrenko@gmail.com>
2015-11-15 18:22:01 +0100

    remove obsolete check

--- dsis/src/UIs/UI.Application/src/Program.cs
+++ dsis/src/UIs/UI.Application/src/Program.cs
@@ -20,16 +20,7 @@ namespace DSIS.UI.Application
 
       System.Windows.Forms.Application.EnableVisualStyles();
       System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
-      try
-      {
-        if (Assembly.Load(new AssemblyName("System.Core, Version=3.5.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")) == null)
-          throw new Exception("Null");
-      } catch
-      {
-        MessageBox.Show("Failed to find Microsoft .NET Framework 3.5. Please install it from http://download.microsoft.com",
-                        "DSIS Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-        return -1;
-      }
+      
 
       return ApplicationEntryPoint<ApplicationClass>.DoMain(args);
     }
